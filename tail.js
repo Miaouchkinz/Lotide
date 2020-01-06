fb61436ef9ad04830e9148f92ad84b8008d2e19e
Gab Richard <g,elodierichard@gmail.com>
2020-01-06 22:40:50 +0000

    added a function which only shows the tail of an array

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(tailOfArray) {
+  tailOfArray.shift();
+  return tailOfArray;
+};
+
+// TEST 1:
+// An array with only one element should yield an empty array for its tail
+const favoritePets = ["cat"];
+assertEqual(tail(favoritePets).length, 0);
+
+// TEST 2:
+// An empty array should yield an empty array for its tail
+const emptyArray = [];
+assertEqual(tail(emptyArray).length, 0);
