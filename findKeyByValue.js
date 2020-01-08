a6970ee44945efafe3f2e7526353b873809a8230
Gab Richard <g,elodierichard@gmail.com>
2020-01-08 23:25:33 +0000

    created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+// NOTE: Our assertEqual function can only compare primitive values.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, keyValue) {
+  for (let key in object) {
+    if (object[key] === keyValue) {
+      return key;
+    }
+  }
+  return;
+};
+
+// TESTS
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Steven Universe"), undefined);
