f1e436a0e36c177a1f87ac688e2f6975f179561d
Gab Richard <g,elodierichard@gmail.com>
2020-01-09 03:52:56 +0000

    commented out the test code

--- eqArrays.js
+++ eqArrays.js
@@ -23,13 +23,13 @@ const eqArrays  = function(firstArray, secondArray) {
 // Tests
 
 // eqArrays([1, 2, 3], [1, 2, 3]) // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
 // eqArrays([1, 2, 3], [3, 2, 1]) // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 
 // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
