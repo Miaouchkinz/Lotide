9e7d3de9670aa4abda79d632d77aa1db4d44605d
Gab Richard <g,elodierichard@gmail.com>
2020-01-09 03:36:16 +0000

    added the util library inspect function in assertion console log

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -27,25 +27,26 @@ const eqObjects = function(object1, object2) {
 };
 
 
-const assertObjectsEqual = function(object1, object2) {
-  if (eqObjects(object1, object2)) {
-    console.log(`✅✅✅ Assertion Passed: these objects are equal!`);
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: these objects are not equal!`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
 // // TEST
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertObjectsEqual(ab, ba); // => true
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// assertObjectsEqual(ab, abc); // => false
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertObjectsEqual(cd, dc); // => true
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertObjectsEqual(cd, cd2); // => false
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false
