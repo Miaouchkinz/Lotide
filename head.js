9953dacf8dba45d66dad94c3f7e6103b59c922e3
Gab Richard <g,elodierichard@gmail.com>
2020-01-09 03:43:44 +0000

    commented out the test code

--- head.js
+++ head.js
@@ -10,7 +10,9 @@ const head = function(headOfGivenArray){
   return headOfGivenArray[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+// TEST
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
