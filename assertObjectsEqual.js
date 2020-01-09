961a9f7406aef8fbff404fd592874bc718b65101
Gab Richard <g,elodierichard@gmail.com>
2020-01-09 03:13:27 +0000

    create assertObject functions test

---
+++ assertObjectsEqual.js
@@ -0,0 +1,51 @@
+const eqArrays  = function(firstArray, secondArray) {
+  let isSame = true;
+  if (firstArray.length !== secondArray.length) {
+    isSame = false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      isSame = false;
+    }
+  }
+  return isSame;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertObjectsEqual = function(object1, object2) {
+  if (eqObjects(object1, object2)) {
+    console.log(`✅✅✅ Assertion Passed: these objects are equal!`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: these objects are not equal!`);
+  }
+};
+
+// // TEST
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertObjectsEqual(ab, ba); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// assertObjectsEqual(ab, abc); // => false
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertObjectsEqual(cd, dc); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertObjectsEqual(cd, cd2); // => false
