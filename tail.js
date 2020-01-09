68039984f44cc8e9948541ee1677917cf2d9e5ea
Gab Richard <g,elodierichard@gmail.com>
2020-01-09 03:44:14 +0000

    commented out the test code

--- tail.js
+++ tail.js
@@ -13,10 +13,10 @@ const tail = function(tailOfArray) {
 
 // TEST 1:
 // An array with only one element should yield an empty array for its tail
-const favoritePets = ["cat"];
-assertEqual(tail(favoritePets).length, 0);
+// const favoritePets = ["cat"];
+// assertEqual(tail(favoritePets).length, 0);
 
 // TEST 2:
 // An empty array should yield an empty array for its tail
-const emptyArray = [];
-assertEqual(tail(emptyArray).length, 0);
+// const emptyArray = [];
+// assertEqual(tail(emptyArray).length, 0);
