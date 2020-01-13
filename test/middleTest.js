ed05cc719f77189b753041cc7c6f5e33cfa82e2e
Gab Richard <g,elodierichard@gmail.com>
2020-01-13 21:47:31 +0000

    changed the tests to use mochai/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,24 @@
 const middle = require("../middle")
 const assertArraysEqual = require("../assertArraysEqual")
+const assert = require('chai').assert;
 
-// TEST
-// One to two elements, returns empty array
-assertArraysEqual(middle([1]), []) 
-assertArraysEqual(middle([1, 2]), []) 
-// Uneven number returns one element
-assertArraysEqual(middle([1, 2, 3]), [2]) 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) 
-// Even number returns two elements
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) 
+describe("#middle", () => {
+  // One to two elements, returns empty array
+  it("returns empty array when your array has one to two elements", () => {
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  // Uneven number returns one element
+  it("returns one element when your array's length is an uneven number", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+   // Even number returns two elements
+   it("returns two element when your array's length is an even number", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
