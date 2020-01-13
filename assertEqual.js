16a9bfb2531b22b522bc2b63ac3c7dc7cfdb503b
Gab Richard <g,elodierichard@gmail.com>
2020-01-13 17:50:56 +0000

    moved test into test file, added module export with @fredBordel and @DragonMess

--- assertEqual.js
+++ assertEqual.js
@@ -8,9 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("apples", "apples");
-// assertEqual(4, 6);
-
+module.exports = assertEqual;
