2f368d6e2fc3feeda522c5b59aa5e1ce5985d63c
Gab Richard <g,elodierichard@gmail.com>
2020-01-13 20:09:49 +0000

    updated to reflect code changes done with pair programming

--- middle.js
+++ middle.js
@@ -1,24 +1,4 @@
-const eqArrays  = function(firstArray, secondArray) {
-  let isSame = true;
-  if (firstArray.length !== secondArray.length) {
-    isSame = false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      isSame = false;
-    }
-  }
-  return isSame;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 // Returns the midIndex element of a given array
 const middle = function(array) {
@@ -36,16 +16,4 @@ const middle = function(array) {
   return midIndex;
 };
 
-// TEST
-
-// One to two elements, returns empty array
-// assertArraysEqual(middle([1]), []) // => []
-// assertArraysEqual(middle([1, 2]), []) // => []
-
-// Uneven number returns one element
-// assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-
-// Even number returns two elements
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+module.exports = middle;
